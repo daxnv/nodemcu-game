d8eb06c5a99683a3708771adecd9c27b3ada4a19
daxnv <da.nv@web.de>
2021-05-05 17:48:27 +0200

    Vector constructor

--- Vector.h
+++ Vector.h
@@ -8,7 +8,9 @@
 template<typename Type, std::size_t Size>
 class Vector : public std::array<Type, Size> {
 public:
-    using array();
+    Vector(Type ...inits) : std::array{inits,...} {
+
+    }
     
     Vector operator+(const Vector &other) const {
         return binary_op(other, std::plus)
