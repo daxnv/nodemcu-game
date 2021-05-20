014a7d66c9de671a3bd231775ae4157b494464ee
daxnv <da.nv@web.de>
2021-05-20 08:34:00 +0200

    Add Game::{levelUp(), _counter}

--- src/Game.cpp
+++ src/Game.cpp
@@ -16,29 +16,49 @@ Piece random_piece() {
 }
 
 Game::Game() :
-    _piece(random_piece().move({_start_pos, 0})),
+    _piece(moveToStart(random_piece())),
     _preview(random_piece()),
     _board(),
-    _points(0)
+    _points(0),
+    _counter(0)
 {
 }
 
-void Game::cycle() {
+void Game::cycleDown() {
     Piece moved_piece = _piece.moved({0,1});
     if (_board.collision(moved_piece)) {
-        _board.addPiece(_piece);
-
-        int n = _board.deleteLines();
-        _points += n * n;
-
-        _piece = moveToStart(_preview);
-        _preview = random_piece();
+        levelUp();
     }
     else {
         _piece = moved_piece;
     }
 }
 
-Piece &Game::moveToStart(Piece &piece) {
+void Game::levelUp() {
+    _board.addPiece(_piece);
+
+    int n = _board.deleteLines();
+    _points += n * n;
+    ++_counter;
+
+    _piece = moveToStart(_preview);
+    _preview = random_piece();
+}
+
+void Game::cycleUser() {
+    Piece changed_piece = _piece;
+
+    if (_controller.rotate())
+        changed_piece.rotate();
+
+    IntVec shift{0,0};
+    shift[0] = _controller.shift();
+    changed_piece.move(shift);
+
+    if (!_board.collision(changed_piece))
+        _piece = changed_piece;
+}
+
+Piece &Game::moveToStart(Piece piece) {
     return piece.move({_start_pos, 0});
 }
--- src/Game.h
+++ src/Game.h
@@ -2,16 +2,21 @@
 #define _GAME_H
 
 #include "Tetris.h"
+#include "Input.h"
 
 class Game {
 public:
     Game();
-    void cycle();
+    void levelUp();
+    void cycleDown();
+    void cycleUser();
 private:
-    static Piece &moveToStart(Piece &piece);
+    static Piece &moveToStart(Piece piece);
+
+    Input _controller;
     Piece _piece, _preview;
     Board _board;
-    int _points;
+    int _points, _counter;
     static constexpr int _start_pos = Board::width / 2;
 };
 
