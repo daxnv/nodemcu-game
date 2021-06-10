f40ed9f14570dffb269e7f952544879a8a95e3cb
Yannis Storrer <yannis.storrer@web.de>
2021-06-10 09:25:03 +0200

    Added Output Class

--- src/Game.h
+++ src/Game.h
@@ -1,39 +1,50 @@
 #ifndef _GAME_H
 #define _GAME_H
 
-#include "Tetris.h"
-#include "Wire.h"
 #include "MPU9250.h"
 #include "Tetris.h"
+#include "Wire.h"
 
 class Game {
-public:
-    Game();
-    void cycleDown();
-    void cycleUser();
-    void levelUp();
-    void loop();
-    void go();
-private:
+ public:
+  Game();
+  void cycleDown();
+  void cycleUser();
+  void levelUp();
+  void loop();
+  void go();
+  class Output {
+   public:
+    Output(int width, int heigh);
+    void draw();
 
-    class Input {
-    public:
-        Input();
-        void measure();
-        int shift();
-        bool rotation();
-    private:
-        MPU9250 Sensor;
-        bool last_was_rot;
-    };
+   private:
+    int width;
+    int heigh;
+    TFT_eSPI tft;
+  };
+  Output display;
 
-    static Piece &moveToStart(Piece piece);
+ private:
+  class Input {
+   public:
+    Input();
+    void measure();
+    int shift();
+    bool rotation();
 
-    Input _controller;
-    Piece _piece, _preview;
-    Board _board;
-    int _points, _counter;
-    static constexpr int _start_pos = Board::width / 2;
+   private:
+    MPU9250 Sensor;
+    bool last_was_rot;
+  };
+
+  static Piece &moveToStart(Piece piece);
+
+  Input _controller;
+  Piece _piece, _preview;
+  Board _board;
+  int _points, _counter;
+  static constexpr int _start_pos = Board::width / 2;
 };
 
-#endif //_GAME_H
+#endif  //_GAME_H
